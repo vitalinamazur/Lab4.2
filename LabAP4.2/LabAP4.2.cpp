a3644e250157d50c44af4869e5012d92ed05ea15
Vitalina <vitalinamazur296@gmail.com>
2024-09-28 11:45:45 +0300

    commit_1

---
+++ LabAP4.2/LabAP4.2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 2 * x - 13,5;
+		if (x < -1)
+			B = sin(x) / (1 + cos(x * x));
+		else
+			if (-1 <= x && x <= 1)
+				B = pow(cos(pow(sin(x), 2)), 2) - 1;
+			else
+				B = log10(x + 0.4);
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
